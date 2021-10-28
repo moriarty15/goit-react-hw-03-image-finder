dac680f0d911e173b5f97d33cf30496767bd3641
Vladimir Palamov <84524771+moriarty15@users.noreply.github.com>
2021-10-28 11:31:17 +0800

    rafactor

--- src/App.js
+++ src/App.js
@@ -4,30 +4,44 @@ import Searchbar from "./Components/Searchbar";
 import ImageGallery from "./Components/ImageGallery";
 import Button from "./Components/Button";
 import Modal from "./Components/Modal";
+import Loader from "./Components/Loader";
 
 class App extends Component {
   state = {
+    images: [],
     query: "",
     page: 1,
     showModal: false,
+    showSpiner: false,
 
     large: null,
     alt: null,
   };
 
   componentDidMount() {
-    // if (localStorage.getItem)
-    localStorage.setItem("images", "[]");
     window.addEventListener("click", (e) => {
       const findImg = e.target.src;
-      const arrImg = JSON.parse(localStorage.getItem("images"));
-      const findImgforModal = arrImg.find((e) => e.webformatURL === findImg);
+      const findImgforModal = this.state.images.find(
+        (e) => e.webformatURL === findImg
+      );
       if (findImgforModal) {
         this.setState({ large: findImgforModal.largeImageURL });
         this.setState({ alt: findImgforModal.user });
         this.setState({ showModal: true });
       }
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.query !== this.state.query) {
+      this.setState({ images: [] });
+      this.fetchRequest();
+    } else if (
+      this.state.query.trim() !== "" &&
+      this.state.page !== prevState.page
+    ) {
+      this.fetchRequest();
+    }
   }
 
   onClose = () => {
@@ -46,15 +60,46 @@ class App extends Component {
     });
   };
 
+  fetchRequest = () => {
+    this.setState({ showSpiner: true });
+    fetch(
+      `https://pixabay.com/api/?q=${this.state.query}&page=${this.state.page}&key=23320531-e67f94e9f6229e6b46894ace7&image_type=photo&orientation=horizontal&per_page=12`
+    )
+      .then((r) => r.json())
+      .then((hit) => {
+        if (hit.total === 0) {
+          this.setState({ showSpiner: false });
+          alert(
+            "По данному запросу ничего не найдено, сделайте запрос более специфичным"
+          );
+          return;
+        }
+        this.setState({ images: [...this.state.images, ...hit.hits] });
+        this.setState({ showSpiner: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: "smooth",
+        });
+      })
+      .catch((e) => {
+        alert("Ой, что-то пошло не так");
+      });
+  };
+
   render() {
-    const { showModal, large, alt, query } = this.state;
+    const { showModal, large, alt, images, showSpiner } = this.state;
     return (
       <div className={style.App}>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery query={this.state.query} page={this.state.page} />
+        <ImageGallery images={images} />
 
-        {query !== "" && <Button onClick={this.clickLoadMore} />}
+        {images.length >= 12 && <Button onClick={this.clickLoadMore} />}
         {showModal && <Modal src={large} onClose={this.onClose} alt={alt} />}
+        {showSpiner && (
+          <div className={style.centred}>
+            <Loader />
+          </div>
+        )}
       </div>
     );
   }
--- src/Components/ImageGallery/ImageGallery.js
+++ src/Components/ImageGallery/ImageGallery.js
@@ -1,79 +1,24 @@
-import { Component } from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 import style from "./ImageGallery.module.css";
 import ImageGalleryItem from "../ImageGalleryItem";
-import LoaderFoo from "../Loader";
 
-export default class ImageGallery extends Component {
-  state = {
-    isDone: false,
-    showSpiner: false,
-  };
-
-  static propTypes = {
-    query: PropTypes.string.isRequired,
-    page: PropTypes.number.isRequired,
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.query !== this.props.query) {
-      localStorage.setItem("images", "[]");
-      this.fetchRequest();
-    } else if (
-      this.props.query.trim() !== "" &&
-      this.props.page !== prevProps.page
-    ) {
-      this.fetchRequest();
-    }
-  }
-
-  fetchRequest = () => {
-    this.setState({ showSpiner: true });
-    fetch(
-      `https://pixabay.com/api/?q=${this.props.query}&page=${this.props.page}&key=23320531-e67f94e9f6229e6b46894ace7&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then((r) => r.json())
-      .then((hit) => {
-        if (hit.total === 0) {
-          alert("По данному запросу ничего не найдено, сделайте запрос более специфичным")
-        }
-        const saveImg = JSON.parse(localStorage.getItem("images"));
-        saveImg.push(...hit.hits);
-        localStorage.setItem("images", JSON.stringify(saveImg));
-        this.setState({ showSpiner: false });
-        this.setState({ isDone: true });
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: "smooth",
-        });
-      }).catch(e => {
-        alert('Ой, что-то пошло не так')
-      });
-  };
-
-  render() {
-    const images = JSON.parse(localStorage.getItem("images"));
-    return (
-      <>
-        {this.state.isDone && (
-          <ul className={style.ImageGallery}>
-            {images.length !== 0 &&
-              images.map(({id, webformatURL, user}) => (
-                <li key={id}>
-                  <ImageGalleryItem
-                  src={webformatURL}
-                  alt={user}
-                />
-                </li>
-              ))}
-          </ul>
-        )}
-        {this.state.showSpiner && (
-          <div className={style.centred}>
-            <LoaderFoo />
-          </div>
-        )}
-      </>
-    );
-  }
+export default function ImageGallery({ images }) {
+  return (
+    <>
+      {
+        images.length !== 0 && <ul className={style.ImageGallery}>
+          {
+            images.map(({ id, webformatURL, user }) => (
+              <li key={id}>
+                <ImageGalleryItem src={webformatURL} alt={user} />
+              </li>
+            ))}
+        </ul>
+      }
+    </>
+  );
 }
+
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(PropTypes.object),
+};
